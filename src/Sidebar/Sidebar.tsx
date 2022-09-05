f4a7b01b475460302041dedcd1d4090ed61a42d2
Eric Newton <ericnewton@Erics-MacBook-Air.local>
2022-09-04 17:08:37 -0700

    Added sidebar and fonts, sidebar rerenders the whole app

--- src/Sidebar/Sidebar.tsx
+++ src/Sidebar/Sidebar.tsx
@@ -1,5 +1,26 @@
-export default function Sidebar() {
+import { useState } from "react"
+
+export type SidebarToggleProps = {
+    onToggle: ( opened: boolean ) => void;
+}
+
+export type SidebarProps = {
+    opened: boolean;
+}
+
+export function SidebarToggle(props: SidebarToggleProps) {
+    const [state, setState] = useState(false);
+    const onClick = () => {
+        setState(!state);
+        props.onToggle(state);
+    }
+
     return (
-        <button>Open</button>
+        <button onClick={onClick}>{state ? 'Close' : 'Open'}</button>
     )
+}
+
+export function Sidebar(props: SidebarProps) {
+    
+    return props.opened ? <div>Sidebar</div> : <div></div>
 }
--- src/app-main/AppMain.tsx
+++ src/app-main/AppMain.tsx
@@ -1,8 +1,26 @@
-import Sidebar from "../Sidebar/Sidebar";
+import { useState } from "react";
+import { Sidebar, SidebarToggle } from "../Sidebar/Sidebar";
+
+type AppState = {
+    sidebarOpen: boolean;
+}
 
 function AppMain() {
+    const [state, setState] = useState<AppState>({sidebarOpen: false})
+    const onSidebarToggle = (sidebarOpen: boolean) => {
+        setState({...state, sidebarOpen});
+    }
+
+    console.log('App main rerender');
     return (
-        <Sidebar/>
+        <div>
+            <div>
+                <Sidebar opened={state.sidebarOpen}/>
+            </div>
+            <div>
+                <SidebarToggle onToggle={onSidebarToggle}/>
+            </div>
+        </div>
     )
 }
 
