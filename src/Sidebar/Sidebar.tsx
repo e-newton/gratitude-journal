e58542a7bd19d83f4aaede2ba28d8588a045b183
Eric Newton <ericnewton@Erics-MacBook-Air.local>
2022-09-04 18:05:05 -0700

    Added sidebar

--- src/Sidebar/Sidebar.tsx
+++ src/Sidebar/Sidebar.tsx
@@ -1,26 +1,45 @@
 import { useState } from "react"
+import './Sidebar.scss';
 
 export type SidebarToggleProps = {
     onToggle: ( opened: boolean ) => void;
+    opened: boolean;
 }
 
-export type SidebarProps = {
+export type SidebarState = {
     opened: boolean;
 }
 
 export function SidebarToggle(props: SidebarToggleProps) {
-    const [state, setState] = useState(false);
     const onClick = () => {
-        setState(!state);
-        props.onToggle(state);
+        props.onToggle(!props.opened);
     }
 
     return (
-        <button onClick={onClick}>{state ? 'Close' : 'Open'}</button>
+        <button className="toggle" onClick={onClick}>{props.opened ? 'Close' : 'Open'}</button>
     )
 }
 
-export function Sidebar(props: SidebarProps) {
-    
-    return props.opened ? <div>Sidebar</div> : <div></div>
+export function Sidebar() {
+
+    const [state, setState] = useState<SidebarState>({opened: false});
+
+    const onSidebarToggle = (sidebarToggle: boolean) => {
+        setState({...state, opened: sidebarToggle});
+    }
+
+    const getClassName = () => {
+        const classes = ['sidebar'];
+        state.opened && classes.push('opened');
+        return classes.join(' ');
+    }
+
+    return (
+        <>
+            <div className={getClassName()}>
+                {state.opened ? 'opened' : 'closed'}
+                <SidebarToggle onToggle={onSidebarToggle} opened={state.opened}/>
+            </div>
+        </>
+    )
 }
--- src/app-main/AppMain.tsx
+++ src/app-main/AppMain.tsx
@@ -6,20 +6,10 @@ type AppState = {
 }
 
 function AppMain() {
-    const [state, setState] = useState<AppState>({sidebarOpen: false})
-    const onSidebarToggle = (sidebarOpen: boolean) => {
-        setState({...state, sidebarOpen});
-    }
-
     console.log('App main rerender');
     return (
         <div>
-            <div>
-                <Sidebar opened={state.sidebarOpen}/>
-            </div>
-            <div>
-                <SidebarToggle onToggle={onSidebarToggle}/>
-            </div>
+            <Sidebar/>
         </div>
     )
 }
